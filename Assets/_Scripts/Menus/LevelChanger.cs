17ec8a660ed155a8f14d59b677b3b8cd260271ce
Liam Sharkey <l.sharkey1105@hotmail.com>
2024-03-30 21:11:11 -0400

    various things
    Added boss sequencing so the mirror works, updated all menu buttons to have sounds and moved some of the menuing stuff around, changed total number of bosses and fixed reset

--- Assets/_Scripts/Menus/LevelChanger.cs
+++ Assets/_Scripts/Menus/LevelChanger.cs
@@ -8,17 +8,8 @@ public class LevelChanger : MonoBehaviour
     public Animator animator;
     private string levelToLoad;
 
-    public AudioSource source;
-
     public void FadeToLevel(string sceneName)
     {
-        animator.SetTrigger("FadeOut");
-        levelToLoad = sceneName;
-    }
-
-    public void fadeToLevelWithSelectNoise(string sceneName)
-    {
-        source.Play();
         animator.SetTrigger("FadeOut");
         levelToLoad = sceneName;
     }
