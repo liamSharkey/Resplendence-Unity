f5c50e8f55a79169f5e222ec46afdd9865e80d7e
Liam Sharkey <l.sharkey1105@hotmail.com>
2024-03-31 00:06:26 -0400

    updated total game bosses to 3

--- Assets/_Scripts/_GameManager.cs
+++ Assets/_Scripts/_GameManager.cs
@@ -7,7 +7,7 @@ public class _GameManager : MonoBehaviour
 {
     public static int highestBossDefeated;
     public static int numberOfDefeatedBosses;
-    public static int totalNumberOfBosses = 2;
+    public static int totalNumberOfBosses = 3;
 
     public static bool isPaused = false;
 
